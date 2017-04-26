ec70da69bebb34d15ab3411bd09fc5bd34a2b494
Tim <timcocu@gmail.com>
2017-04-26 13:42:42 +0200

    Add rough TypeScript setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,28 @@
-// Gulp!
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var tsify = require('tsify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
+
+// Based on the docs at:  https://www.typescriptlang.org/docs/handbook/gulp.html
+gulp.task('default', function () {
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/main.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify)
+    .transform('babelify', {
+        presets: ['es2015'],
+        extensions: ['.ts']
+    })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'));
+});
